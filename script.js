6c25d60080071ecf68671482a7dccbc89ebdfa58
davitJabushanuri <davitjabushanuri95@gmail.com>
2021-12-20 02:19:11 +0400

    make array render in html

--- script.js
+++ script.js
@@ -12,13 +12,38 @@
 
 // console.log(harryPotter.info());
 
+
 const booksArray = [
 	{title: 'Hobbit',
 	author: 'JK WOELH',
 	pages: 200,
-	read:true	
-}
+	read: true	
+	},
+	{title: 'Hobfdfbit',
+	author: 'JK WOELdsafdfH',
+	pages: 300,
+	read: false	
+	}
 ];
+
+booksArray.forEach(book =>{
+	let books = document.createElement('div')
+	books.innerHTML = `
+	<ul>
+		<li>${book.title}</li>
+		<li>${book.author}</li>
+		<li>${book.pages}</li>
+		<li>${book.read}</li>
+	</ul>
+	<button>Read</button>
+	<button>Delete</button>`
+	books.classList.add('book-style')
+	
+	
+	document.querySelector('.book-container').appendChild(books)
+
+})
+
 
 const addButton = document.getElementById('add-book-button')
 addButton.addEventListener('click', ()=>{
