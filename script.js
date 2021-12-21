4a339faed29596cef4789d2c98895c7051ac071c
davitJabushanuri <davitjabushanuri95@gmail.com>
2021-12-21 18:29:19 +0400

    now you can use form to add books to the library

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
-// function book(title, author, pages, read) {
-// 	this.title = title;
-// 	this.author = author;
-// 	this.pages = pages;
-// 	this.read = read;
-// 	this.info = function () {
-// 		return `${title}, ${author}, ${pages}, ${read}`;
-// 	};
-// }
+const form = document.querySelector('.form')
+const titleInput = document.querySelector('#book-title')
+const authorInput = document.querySelector('#book-author')
+const pagesInput = document.querySelector('#book-pages')
+const submitButton = document.querySelector('#submit')
+const readCheckbox = document.querySelector('#read')
+const bookContainer = document.querySelector('.book-container')
 
-// const harryPotter = new book(`Harry Potter`, `J.K. Rowling`, 400, true);
-
-// console.log(harryPotter.info());
-
-
-const booksArray = [
+const myLibrary = [
 	{title: 'Hobbit',
-	author: 'JK WOELH',
+	author: 'Tolkien',
 	pages: 200,
 	read: true	
 	},
-	{title: 'Hobfdfbit',
-	author: 'JK WOELdsafdfH',
+	{title: 'Harry Potter',
+	author: 'Rawlings',
 	pages: 300,
 	read: false	
 	}
 ];
 
-booksArray.forEach(book =>{
+function Book (title, author, pages, read) {
+	this.title = title
+	this.author = author
+	this.pages = pages
+	this.read = read
+}
+
+const addBookToLibrary = () => {
+let title = titleInput.value
+let author = authorInput.value
+let pages = pagesInput.value
+let read = readCheckbox.value
+let newBook = new Book(title, author, pages, read)
+myLibrary.push(newBook)
+}
+
+
+const updateLibrary = () => {
+bookContainer.innerHTML = ''
+myLibrary.forEach(book => {
 	let books = document.createElement('div')
 	books.innerHTML = `
 	<ul>
@@ -40,13 +52,28 @@ booksArray.forEach(book =>{
 	books.classList.add('book-style')
 	
 	
-	document.querySelector('.book-container').appendChild(books)
+	bookContainer.appendChild(books)
 
 })
+}
+
+const clearForm = () => {
+	title.input.value = ''
+	authorInput.value = ''
+	pagesInput.value = ''
+}
 
 
 const addButton = document.getElementById('add-book-button')
 addButton.addEventListener('click', ()=>{
 	let x = document.querySelector('form')
 	x.classList.toggle('show-hide')
+})
+
+submitButton.addEventListener('click', (e) => {
+	e.preventDefault()
+	addBookToLibrary()
+	updateLibrary()
+	clearForm()
+	console.log(myLibrary);
 })
